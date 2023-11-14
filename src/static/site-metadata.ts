0212a4510c87bdab06410d78e1c15c7f0d6dd38d
Haozhe-Li <116460778+Haozhe-Li@users.noreply.github.com>
2023-11-14 06:12:12 +0000

    update

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: '运动',
+  siteUrl: 'https://run.haozheli.com',
+  logo: 'https://www.haozheli.com/assets/img/apple-touch-icon.png',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'Github',
+      url: 'https://github.com/Haozhe-Li/run',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://haozheli.com',
     },
   ],
 };
